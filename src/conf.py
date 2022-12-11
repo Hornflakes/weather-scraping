8f2ad7a87eb167b65ed323c2a59c3f0ef325a467
Hornflakes <cristianpetrugug@gmail.com>
2022-12-11 21:55:06 +0200

    feat: add getting data excel configuration from config excel

--- src/conf.py
+++ src/conf.py
@@ -1,3 +1,12 @@
-EXCEL_FILE_NAME = "temperaturi.xlsx"
-EXCEL_SHEET_NAME = "2022"
-DATE_COL_EXCEL_INDEX = 184
+import pandas as pd
+
+
+configDf = pd.read_excel(
+    io="config.xlsx",
+    sheet_name="Config",
+    converters={"EXCEL_SHEET_NAME": str, "DATE_COLUMN_EXCEL_INDEX": int},
+)
+
+EXCEL_FILE_NAME = configDf["EXCEL_FILE_NAME"][0] + ".xlsx"
+EXCEL_SHEET_NAME = configDf["EXCEL_SHEET_NAME"][0]
+DATE_COL_EXCEL_INDEX = configDf["DATE_COLUMN_EXCEL_INDEX"][0]
